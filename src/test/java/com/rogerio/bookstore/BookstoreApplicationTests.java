a1bff1dc9c3bed7748882306874069036eb21dc9
= <=>
2021-10-18 14:12:29 -0300

    commit inical

---
+++ src/test/java/com/rogerio/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerio.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
