0e3e9fa695a1bfa8f451691fbd4fa2bcfcab538b
= <=>
2021-10-21 17:03:40 -0300

    deployando

---
+++ src/main/java/com/rogerio/bookstore/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.rogerio.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.rogerio.bookstore.service.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instanciaBaseDeDados() {
+        dbService.instanciaBaseDeDados();
+    }
+}
